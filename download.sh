59289692af6d2c30be3b1683b08af891f2154cf3
Xsoft <127049334+NoltixOS@users.noreply.github.com>
2025-06-15 21:08:59 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,6 +7,22 @@ set -eu
 # and customized Arch Linux system.
 # Copyright (C) 2022 picodotdev
 
+function warning() {
+     echo -e "${PURPLE}Welcome to the stellarOS installation${NC}"
+        echo -e "${BLUE}this will patch Arch Linux to run stellarOS${NC}"
+        echo -e "${BLUE}It will load an automated script.${NC}"
+        echo -e "${BLUE}After this, edit the alis.conf file so that you can customize (For example, choosing what DE the script will install, the account etc.)${NC}"
+        echo -e "${BLUE}then run ./alsi.sh${NC}"
+    echo ""
+    echo -e "${RED}Warning"'!'"${NC}"
+    echo -e "${RED}This script can delete all partitions of the persistent${NC}"
+    echo -e "${RED}storage and continuing all your data can be lost.${NC}"
+    echo ""
+    echo -e "Install device: $DEVICE."
+    echo -e "Mount points: ${PARTITION_MOUNT_POINTS[*]}."
+    echo ""
+read -p "Press [Enter] to install scripts..."
+
 GITHUB_USER="picodotdev"
 BRANCH="main"
 HASH=""
